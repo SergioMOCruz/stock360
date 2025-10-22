5e85c79016c5f241d710ce7633dccfe977858c6e
Sergio <cruzmosergio@gmail.com>
2025-10-22 22:52:55 +0100

    Add model to users service

--- api-stock360/services/users-service/app/models.py
+++ api-stock360/services/users-service/app/models.py
@@ -25,3 +25,9 @@ class UserUpdate(BaseModel):
     name: Optional[str] = None
     email: Optional[EmailStr] = None
     role: Optional[str] = None
+
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    email: str
